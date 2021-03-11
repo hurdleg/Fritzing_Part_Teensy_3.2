7b27c9ce0c0f9dcd046c8fb25be4f49dc769a89e
Gerald C. Hurdle <Gerald.Hurdle@AlgonquinCollege.com>
2021-03-10 22:52:14 -0500

    FIX: changed to ZIP from TAR.

--- bin/mk_fzpz.py
+++ bin/mk_fzpz.py
@@ -6,38 +6,38 @@ from shutil import copyfile
 PART = "Teensy_3.2"
 PART_FILE = str(PART + ".fzp")
 PART_BIN = str(PART + ".fzpz")
-PART_TAR = str("./part." + PART_FILE)
+PART_ZIP = str("./part." + PART_FILE)
 BREADBOARD = str(PART + "_breadboard.svg")
-BREADBOARD_TAR = str("./svg.breadboard." + BREADBOARD)
+BREADBOARD_ZIP = str("./svg.breadboard." + BREADBOARD)
 ICON = str(PART + "_icon.svg")
-ICON_TAR = str("./svg.icon." + ICON)
+ICON_ZIP = str("./svg.icon." + ICON)
 PCB = str(PART + "_pcb.svg")
-PCB_TAR = str("./svg.pcb." + PCB)
+PCB_ZIP = str("./svg.pcb." + PCB)
 SCHEMATIC = str(PART + "_schematic.svg")
-SCHEMATIC_TAR = str("./svg.schematic." + SCHEMATIC)
-TAR_CMD = ["tar", "-czf", PART_BIN, PART_TAR, BREADBOARD_TAR, ICON_TAR, PCB_TAR, SCHEMATIC_TAR]
+SCHEMATIC_ZIP = str("./svg.schematic." + SCHEMATIC)
+ZIP_CMD = ["zip", PART_BIN, PART_ZIP, BREADBOARD_ZIP, ICON_ZIP, PCB_ZIP, SCHEMATIC_ZIP]
 
 # Part
-copyfile(str("./core/part/" + PART_FILE), PART_TAR)
+copyfile(str("./core/part/" + PART_FILE), PART_ZIP)
 
 # Breadboard
-copyfile(str("./core/svg/breadboard/" + BREADBOARD), BREADBOARD_TAR)
+copyfile(str("./core/svg/breadboard/" + BREADBOARD), BREADBOARD_ZIP)
 
 # ICON
-copyfile(str("./core/svg/icon/" + ICON), ICON_TAR)
+copyfile(str("./core/svg/icon/" + ICON), ICON_ZIP)
 
 # PCB
-copyfile(str("./core/svg/pcb/" + PCB), PCB_TAR)
+copyfile(str("./core/svg/pcb/" + PCB), PCB_ZIP)
 
 # SCHEMATIC
-copyfile(str("./core/svg/schematic/" + SCHEMATIC), SCHEMATIC_TAR)
+copyfile(str("./core/svg/schematic/" + SCHEMATIC), SCHEMATIC_ZIP)
 
 # make the Fritzing .FZPZ file
-subprocess.call(TAR_CMD)
+subprocess.call(ZIP_CMD)
 
 # clean-up unwanted files
-os.remove(PART_TAR)
-os.remove(BREADBOARD_TAR)
-os.remove(ICON_TAR)
-os.remove(PCB_TAR)
-os.remove(SCHEMATIC_TAR)
+os.remove(PART_ZIP)
+os.remove(BREADBOARD_ZIP)
+os.remove(ICON_ZIP)
+os.remove(PCB_ZIP)
+os.remove(SCHEMATIC_ZIP)
